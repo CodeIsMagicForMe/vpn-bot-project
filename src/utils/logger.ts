d9d5b3a6f09eb2627808ff246e1ec374d2d5633c
CodeIsMagicForMe <129680205+CodeIsMagicForMe@users.noreply.github.com>
2025-11-30 01:51:44 +0300

    Update logger.ts

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,1 +1,24 @@
+// src/utils/logger.ts
+import pino from "pino";
+import { config } from "./config";
 
+const isProd = config.logging.environment === "production";
+
+const logger = pino(
+  isProd
+    ? {
+        level: config.logging.level,
+      }
+    : {
+        level: config.logging.level,
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            translateTime: "SYS:standard",
+          },
+        },
+      },
+);
+
+export { logger };
