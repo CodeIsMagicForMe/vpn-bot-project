47856aef2ad79125ace78caa08fa206b591c23e7
CodeIsMagicForMe <129680205+CodeIsMagicForMe@users.noreply.github.com>
2025-11-30 01:52:20 +0300

    Update prisma.ts

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,1 +1,18 @@
+// src/db/prisma.ts
+import { PrismaClient } from "@prisma/client";
+import { logger } from "../utils/logger";
+
+export const prisma = new PrismaClient({
+  log: ["error", "warn"],
+});
+
+prisma
+  .$connect()
+  .then(() => logger.info("Prisma connected to database"))
+  .catch((err) => {
+    logger.error({ err }, "Failed to connect Prisma");
+    process.exit(1);
+  });
+
+export type PrismaTx = Parameters<Parameters<typeof prisma.$transaction>[0]>[0];
 
